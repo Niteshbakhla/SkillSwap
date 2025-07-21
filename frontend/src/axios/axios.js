b3727400b5e0a9d4772f9c463419fa6231df9ff3
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-07-21 19:04:33 +0530

    added routes

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosinstance = axios.create({
-            baseURL: `${import.meta.env.VITE_API_URL}/api`,
+            baseURL: `https://skillswap-6weo.onrender.com/api`,
             withCredentials: true
 })
 
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client"
 
-const socket = io(import.meta.env.VITE_API_URL,
+const socket = io(`https://skillswap-6weo.onrender.com`,
             {
                         withCredentials: true,
             }
