378c4be1ef1f2f51b6a0bd22af1987771d24b799
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-07-21 19:30:08 +0530

    added env file

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,10 @@ import config from "./src/config/config.js";
 const server = createServer(app)
 const io = new Server(server, {
             cors: {
-                        origin: config.NODE_ENV === "production" ? "https://skill-swap-lpnt.vercel.app" : config.LOCAL_HOST,
+                        origin: config.NODE_ENV === "production" ? config.CLIENT_URL : config.LOCAL_HOST,
                         credentials: true,
                         methods: ["GET", "POST"],
-                        
+
             }
 });
 
