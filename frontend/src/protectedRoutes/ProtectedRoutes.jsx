cf2dc3d7d1e672474ae98ed305163d7e686fcbdb
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-01 17:23:31 +0530

    fix(auth): show toast on unauthorized access

--- frontend/src/protectedRoutes/ProtectedRoutes.jsx
+++ frontend/src/protectedRoutes/ProtectedRoutes.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoutes = ({ children }) => {
             const { isLogin, user } = useSelector(state => state.auth);
+            console.log(isLogin, user)
 
             if (!isLogin && !user) {
-                        <Navigate to={"/login"} replace />
+                        toast.error("You must be logged in to continue.")
+                        return <Navigate to={"/login"} replace />
             }
             return children
 }
